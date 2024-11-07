895b03f2a9023e1432144331342e96c8246984f9
Michael Kim <young.kim@spa.com>
2024-11-07 09:42:04 -0500

    adding

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import VerticalCarousel from './VerticalCarousel.jsx'
+
 const Projects = () => {
   return (
     <section>
       <div className="container text-center">
         <div className="flex flex-col justify-center items-center">
-          <h1 className="font-bold text-5xl">Timeline</h1>
+          <h1 className="font-bold text-5xl">Projects</h1>
           <br />
           <p>My journey</p>
         </div>
     
-        {/* <div classNameName="grid grid-cols-3 justify-center items-center">
-          <div classNameName="service-thumb">content 1</div>
+        <div className="grid grid-cols-3 justify-center items-center">
+          <div className="service-thumb">content 1</div>
           <div>content 2</div>
           <div>content 3</div>
           <div>content 4</div>
           <div>content 5</div>
           <div>content 6</div>
-        </div> */}
+        </div>
       </div>
     </section>
   );
