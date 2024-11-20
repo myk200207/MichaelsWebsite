ad11645fdee84570ba980ea23ba7c44b0942d630
Michael Kim <young.kim@spa.com>
2024-11-20 14:25:46 -0500

    testing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
-import ProfilePic from '../assets/ProfilePic2.JPG'
+import ProfilePic from '../assets/ProfilePic2.jpg'
 
 const Hero = () => {
     return (
       <>
         <section className="h-screen" id="LandingPage">
           <div className=" flex w-screen h-screen justify-center item-center ">
-            {/* style={{backgroundImage: `url(${backgroundImage}`, backgroundSize:'cover', backgroundPosition:'center', imageRendering:'auto' }}> */}
             <div className=" text-white flex flex-col justify-center items-center text-center gap-10 flex-1">
               <div
                 className="w-60 h-60 overflow-hidden rounded-full"
