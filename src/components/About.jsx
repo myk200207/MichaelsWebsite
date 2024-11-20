8963688a536021a280c23032ee5cab17a1406798
Michael Kim <young.kim@spa.com>
2024-11-20 14:06:30 -0500

    clean

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import AboutMePhoto from '../assets/aboutme.jpg'
 const About = () => {
   return (
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import {useState} from 'react'
 import resume from '../assets/resume.pdf'
 
 const NavBar = () => {
