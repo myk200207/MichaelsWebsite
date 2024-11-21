b598cc3cc36ce942deda3a44703b848a8ccaf61a
Michael Kim <young.kim@spa.com>
2024-11-21 09:32:27 -0500

    saving

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MichaelsWebsite/",  
+  base: "./",  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
